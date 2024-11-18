0a85953fc3b0ab6e42ed2f270490cb01095aa22d
Trong Dao <trongdao20402@gmail.com>
2024-11-18 16:42:42 +0700

    Xay dung UI phan Header #4

---
+++ src/components/Popper/Menu/MenuItems.js
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind';
+import Button from '~/components/Layout/components/Button';
+import styles from './Menu.module.scss';
+const cx = classNames.bind(styles);
+function MenuItems({ data }) {
+    return (
+        <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
+            {data.title}
+        </Button>
+    );
+}
+export default MenuItems;
