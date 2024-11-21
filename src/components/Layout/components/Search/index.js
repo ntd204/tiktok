5679483f173ad31de7ccadb68841ec8c786c0de7
Trong Dao <trongdao20402@gmail.com>
2024-11-21 14:06:07 +0700

    Xay dung logic phan Header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import classNames from 'classnames/bind';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 const cx = classNames.bind(styles);
 function Search() {
@@ -13,6 +14,7 @@ function Search() {
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
+    const debounced = useDebounce(searchValue, 500);
     const inputRef = useRef();
     useEffect(() => {
         if (!searchValue.trim()) {
@@ -20,7 +22,7 @@ function Search() {
             return;
         }
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -29,7 +31,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
     const handleClear = () => {
         setSearchValue('');
         setSearchResult([]);
